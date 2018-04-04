0c0e38395bf105742822e1ccc627e80e1199ed3b
angadi.saru <angadi.saru@gmail.com>
2018-04-04 16:56:40 -0400

    test

--- Biblography Project/AddLibrary.php
+++ Biblography Project/AddLibrary.php
@@ -1,15 +1,46 @@
 <?php
-session_start();
+
 include 'config.php';
-include 'signup1.php';
 
-$email = $_POST['email'];
-$name = $_POST['firstname'];
+?>
+<div>
+	<fieldset>
+                <legend><b>Create New Library</b></legend>
+                <form method="POST" id="create_lib_form">
+				Give a name here:
+                    <input type="text" name="Displayname" id="Displayname" value=""  >
+                    <br/>
+											Give a ID:
+                    <input type="text" name="userid" id="userid" value=""  >
+        
+                    <br/>
+                    <input type="submit" value="Create Library" , id = "createlib"/>
+                </form>
+            </fieldset>
 
-$sql = "INSERT INTO library (libraryID, Displayname, userid) 
- VALUES ('', '$name', '$email')";
+</div>
 
-$result = $pdo->query($sql);
 
-header("Location: Bib_Mainpage.php");
+<?
+
+$userid=$_SESSION['userid'];
+if(isset($_POST["action"])){
+
+// creates library
+	if($_POST["action"]=="createlib")
+    {
+		$libname=$_POST["libname"];
+
+		$sql_insert = "INSERT into `Library`
+  			(`Displayname`,'userid'
+			)
+		    VALUES
+		    ('$Displayname','$userid'
+			)
+			";
+
+          mysql_query($sql_insert,$link) or die("Insertion Failed:" . mysql_error());
+		  echo "Library Created, Library ID:".mysql_insert_id();
+		exit();
+	}
 ?>
